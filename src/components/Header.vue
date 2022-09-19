6bd2ca7ea1d01468fe8056dd93f37dd2c8144b7b
Inma Mimateach <inmagdiaz92@gmail.com>
2022-09-19 14:05:19 +0200

    header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,4 @@
-<script setup>
-  
-</script>
+<script setup></script>
 
 <template>
   <main id="header">
@@ -9,16 +7,18 @@
     </div>
     <div id="appTitle">La Tribu Exploradora</div>
     <div id="user">
-      <p>user</p>
-      <img src="./icons/IconUser.vue" />
-      <button id="logout"></button>
+      <div><p>user</p></div>
+      <div><img src="../assets/icons/iconUser.svg" /></div>
+      <div><button id="logout">Salir</button></div>
     </div>
   </main>
 </template>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,600;0,700;0,900;1,400&display=swap");
+
 main #header {
-  background-color: var(--purple-dark);
+  background-color: var(--base-color-purple);
   width: 100%;
   height: 10vh;
   justify-content: space-around;
@@ -29,18 +29,29 @@ main #header {
   flex-direction: row;
   align-items: center;
 }
-#appTitle{
-  font-family: 'Montserrat Alternates', sans-serif;
+#appTitle {
+  font-family: "Montserrat Alternates", sans-serif;
+  font-weight: 700;
+  font-size: 3vh;
+  color: var(--base-color-orange);
 }
 
-#user {
-  justify-content: space-between;
+#user p, #logout {
+  color: var(--base-color-white);
+  font-family: "Raleway", sans-serif;
+  font-weight: 700;
+  font-size: 1.5vh;
+}
+
+#user div {
+  margin: 0 0.5vw 0 0.5vw;
 }
 
 #logout {
   border: none;
-  width: 10vw;
-  height: 2vh;
+  width: 5vw;
+  height: 4vh;
   border-radius: 10vh;
+  background-color: var(--purple-dark);
 }
 </style>
