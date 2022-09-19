8b9e07f71e2f4ff0e9bc0e4ad8b0c2033bba7fb0
Inma Mimateach <inmagdiaz92@gmail.com>
2022-09-19 14:22:07 +0200

    components created

---
+++ src/views/AdminPanelView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "../components/Header.vue";
+import GameList from "../components/GameList.vue";
+import TeacherList from "../components/TeacherList.vue";
+</script>
+<template>
+  <main>
+    <Header />
+    <GameList />
+    <TeacherList />
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: var(--background-color-blue);
+}
+</style>
