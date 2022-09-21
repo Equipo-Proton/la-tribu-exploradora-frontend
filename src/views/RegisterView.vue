1d47d57a209950826fa98299702c17fbc801c582
Majifer <fjmario99@gmail.com>
2022-09-21 14:11:45 +0200

    Register View Added

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,28 +1,16 @@
 <script setup>
-<<<<<<< HEAD
-import { RouterLink, RouterView } from "vue-router";
-import LoginForm from "../components/LoginForm.vue";
-=======
   import { RouterLink, RouterView } from 'vue-router'
   import RegisterForm from '../components/RegisterForm.vue'
->>>>>>> origin/TE-22-Login
 </script>
 <template>
   <header>
     <h1>Este es el formulario de Registro</h1>
   </header>
   <main>
-<<<<<<< HEAD
-    <LoginForm />
-=======
     <RegisterForm/>
->>>>>>> origin/TE-22-Login
   </main>
 </template>
 
-<<<<<<< HEAD
-<style scoped></style>
-=======
 <style scoped>
 h1{
   padding: auto;
@@ -30,4 +18,3 @@ h1{
 
 }
 </style>
->>>>>>> origin/TE-22-Login
