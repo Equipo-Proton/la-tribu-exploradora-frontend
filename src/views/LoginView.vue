e570612243e73977107070090b749d646510066e
Majifer <fjmario99@gmail.com>
2022-09-26 13:42:10 +0200

    Home View with Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import LoginForm from "../components/LoginForm.vue";
+import ButtonsForm from "../components/ButtonsForm.vue";
 </script>
 <template>
   <header>
     <h1>Este es el lugar del Formulario de login</h1>
   </header>
   <main>
-    <LoginForm />
+    <LoginForm/>
+    <ButtonsForm/>
   </main>
 </template>
 
