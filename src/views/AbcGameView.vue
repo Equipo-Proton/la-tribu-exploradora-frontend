e3b27991208a7110075481fc74f1626654424bd6
sudoBuda <juanfranciscobalseca@gmail.com>
2022-09-26 14:38:21 +0200

    abc game view created

---
+++ src/views/AbcGameView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <main></main>
+  <RouterView />
+</template>
+
+<style scoped>
+main {
+  width: 100%;
+  height: 100vh;
+  background-color: var(--background-color-blue);
+  display: flex;
+  justify-content: space-around;
+  padding: 4vh;
+  font-family: "Montserrat Alternates", sans-serif;
+  font-weight: bold;
+  color: var(--font-color);
+}
+</style>
