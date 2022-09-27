2dfc26d7b8fc2113e9521083dff0acfb2444f00d
Inma Mimateach <inmagdiaz92@gmail.com>
2022-09-27 10:27:27 +0200

    routes fixed

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,31 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    //Vista home
+    // vista de login
+
 /*     {
       path: "/",
-      name: "LandingPage",
+      name: "home",
       component: () => import("../views/LandingPageView.vue"),
+    }, */
+/*     {
+      path: "/",
+      name: "Login",
+      component: () => import("../views/LoginView.vue"),
     }, */
     {
       path: "/",
-      name: "Home",
+      name: "home",
       component: () => import("../views/LoginView.vue"),
     },
-    // vista de login
-    {
-      path: "/login",
-      name: "Login",
-      component: () => import("../views/LoginView.vue"),
-    },
-
+    // vista de register
+/*     {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
+    }, */
     // vista de panel de administración
+
     {
       path: "/panel",
       name: "AdminPanel",
@@ -30,12 +36,12 @@ const router = createRouter({
     // vista de prueba simulando la pantalla de espera de los alumnos
     {
       path: "/waiting",
-      name: "WaitingView",
+      name: "Waiting",
       component: () => import("../views/WaitingView.vue"),
     },
     {
       path: "/abcgame",
-      name: "AbcGameView",
+      name: "AbcGame",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -57,7 +63,6 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/GameControlView.vue"),
     },
-
 
 
   ],
--- src/views/AdminPanelView.vue
+++ src/views/AdminPanelView.vue
@@ -26,4 +26,3 @@ main {
   color: var(--font-color);
 }
 </style>
-
