d50db5b1514ef0fd17440b8e449ea0de81f40312
sudoBuda <juanfranciscobalseca@gmail.com>
2022-10-03 09:09:30 +0200

    quit extra login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -37,108 +37,34 @@ export default {
 
 <template>
   <div class="card d-flex">
-    <ul
-      class="nav nav-tabs nav-fill justify-content-around"
-      id="pills-tab"
-      role="tablist"
-    >
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link active"
-          id="pills-user-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-user"
-          type="button"
-          role="tab"
-          aria-controls="pills-user"
-          aria-selected="true"
-        >
-          ALUMNO
-        </button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button
-          class="nav-link"
-          id="pills-teacher-tab"
-          data-bs-toggle="pill"
-          data-bs-target="#pills-teacher"
-          type="button"
-          role="tab"
-          aria-controls="pills-teacher"
-          aria-selected="false"
-        >
-          PROFESOR
-        </button>
-      </li>
-    </ul>
     <div
       class="tab-content d-flex justify-content-center align-items-center m-auto"
       id="pills-tabContent"
     >
-      <div
-        class="tab-pane fade show active m-auto"
-        id="pills-user"
-        role="tabpanel"
-        aria-labelledby="pills-user-tab"
-      >
-        <div class="form">
-          <div>
-            <label for="email">¿Quién eres?</label>
-            <input
-              id="email"
-              type="text"
-              name=""
-              class="form-control"
-              placeholder="E-mail"
-              v-model="form.email"
-            />
-          </div>
-          <div>
-            <label for="password">Contraseña</label>
-            <input
-              id="password"
-              type="password"
-              name=""
-              class="form-control"
-              placeholder="Contraseña"
-              v-model="form.password"
-            />
-          </div>
-          <button class="btn" v-on:click="correctLogin">Entrar</button>
+      <div class="form">
+        <div>
+          <label for="email">¿Quién eres?</label>
+          <input
+            id="email"
+            type="text"
+            name=""
+            class="form-control"
+            placeholder="E-mail"
+            v-model="form.email"
+          />
         </div>
-      </div>
-
-      <div
-        class="tab-pane fade"
-        id="pills-teacher"
-        role="tabpanel"
-        aria-labelledby="pills-teacher-tab"
-      >
-        <div class="form">
-          <div>
-            <label for="email">¿Quién eres?</label>
-            <input
-              id="email"
-              type="text"
-              name=""
-              class="form-control"
-              placeholder="E-mail"
-              v-model="form.email"
-            />
-          </div>
-          <div>
-            <label for="password">Contraseña</label>
-            <input
-              id="password"
-              type="password"
-              name=""
-              class="form-control"
-              placeholder="Contraseña"
-              v-model="form.password"
-            />
-          </div>
-          <button class="btn" v-on:click="correctLogin">Entrar</button>
+        <div>
+          <label for="password">Contraseña</label>
+          <input
+            id="password"
+            type="password"
+            name=""
+            class="form-control"
+            placeholder="Contraseña"
+            v-model="form.password"
+          />
         </div>
+        <button class="btn" v-on:click="correctLogin">Entrar</button>
       </div>
     </div>
   </div>
