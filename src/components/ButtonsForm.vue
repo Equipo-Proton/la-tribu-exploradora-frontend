46a847d9a35c7932368e0da3f88a9eafd418dced
Majifer <fjmario99@gmail.com>
2022-10-03 09:29:28 +0200

    Adding  Game Control Modals

--- src/components/ButtonsForm.vue
+++ src/components/ButtonsForm.vue
@@ -3,25 +3,27 @@
 </script>
 <template>
     <div class="flex-parent jc-center">
-        <button type="button" class="ButtonLeft">Alumno</button>
-        <button type="button" class="ButtonRight">Profesor</button>
+        <button type="button" class="ButtonLeft">Reiniciar Juego</button>
+        <button type="button" class="ButtonRight">Terminar Juego</button>
     </div>
 </template>
 
 <style scoped>
 .ButtonLeft {
-    background-color: cornflowerblue;
-    font-size: 25px;
+    background-color: rgb(255, 183, 0);
+    font-size: 30px;
     border: 0px;
+    height: 60px;
     border-radius: 9px 9px 9px 9px;
     border-color: black;
     padding: auto;
 }
 
 .ButtonRight {
-    background-color: var(--base-color-purple);
-    font-size: 25px;
+    background-color: red;
+    font-size: 30px;
     border: 0px;
+    height: 60px;
     border-radius: 9px 9px 9px 9px;
     border-color: black;
     margin: 6px;
@@ -31,7 +33,7 @@
 
 .flex-parent {
     display: flex;
-    top: 330px;
+    top: 600px;
 
 }
 
@@ -43,12 +45,12 @@
 
 .ButtonLeft:hover {
     color: #fff;
-    background-color: #ab09d0;
+    background-color: #ffbe45;
     border-color: #000000;
 }
 .ButtonRight:hover {
     color: #fff;
-    background-color: #ab09d0;
+    background-color: #ff5b49;
     border-color: #000000;
 }
 </style>
---
+++ src/views/GameControlView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import StudentBox from '../components/StudentBox.vue';
+import ButtonsFormVue from '../components/ButtonsForm.vue';
+import ButtonsForm from '../components/ButtonsForm.vue';
+</script>
+
+<template>
+    <StudentBox/>
+    <ButtonsForm/>
+</template>
