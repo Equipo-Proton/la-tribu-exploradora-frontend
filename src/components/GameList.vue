5a0926b633f9dc077d5a964ad4a3b9591af4113d
Inma Mimateach <inmagdiaz92@gmail.com>
2022-10-04 14:19:29 +0200

    button play/stop placed in the game box

--- src/components/GameList.vue
+++ src/components/GameList.vue
@@ -1,4 +1,77 @@
-<script setup></script>
+<script>
+import { apiTeachers } from "../services/apiTeachers.js";
+import { apiUsers } from "../services/apiUsers.js";
+import { admin } from "../functions/admin.js";
+
+export default {
+  name: "AdminPanelView",
+
+  data() {
+    return {
+      obj: {
+        play: false,
+      },
+      students: [],
+      teachers: [],
+      isAdmin: Boolean,
+      superAdmin: Boolean,
+    };
+  },
+
+  methods: {
+    async listStudents() {
+      const response = await apiUsers.listUsers();
+
+      const studentsData = response.data.data;
+
+      this.students = studentsData;
+    },
+
+    async listTeachers() {
+      const response = await apiTeachers.listTeachers();
+
+      const teachersData = response.data.data;
+
+      this.teachers = teachersData;
+    },
+
+    async play() {
+      if (this.obj.play === false) {
+        this.obj.play = true;
+
+        await apiUsers.play(this.obj);
+
+        return;
+      }
+
+      if (this.obj.play === true) {
+        this.obj.play = false;
+
+        await apiUsers.play(this.obj);
+
+        return;
+      }
+    },
+
+    getAdminValues() {
+      const isAdmin = admin.getIsAdmin();
+
+      const superAdmin = admin.getSuperAdmin();
+
+      this.isAdmin = isAdmin;
+
+      this.superAdmin = superAdmin;
+    },
+  },
+
+  created() {
+    this.listStudents();
+    this.listTeachers();
+    this.getAdminValues();
+  },
+
+};
+</script>
 <template>
   <main>
     <h3>Juegos</h3>
@@ -6,12 +79,29 @@
       <div class="game">
         <img src="../assets/img/gameImage.svg" />
       </div>
+      <div v-if="this.obj.play === false">
+        <button class="play" type="button" v-on:click="play">PLAY</button>
+      </div>
+      <div v-if="this.obj.play === true">
+        <button class="stop" type="button" v-on:click="play">STOP</button>
+      </div>
+      <!--       <div class="playButton" v-if="this.obj.play === true">
+        <button type="button" v-on:click="play">STOP</button>
+      </div>
+      <div class="playButton" v-if="this.obj.play === false">
+        <button type="button" v-on:click="play">PLAY</button>
+      </div> -->
     </div>
   </main>
   <RouterView />
 </template>
 <style scoped>
+
+
 .gamesBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 20vw;
   height: 70vh;
   background: #ffffff;
@@ -30,4 +120,23 @@
   justify-content: center;
   align-items: center;
 }
+
+button {
+  border: none;
+  border-radius: 2vh;
+  height: 7vh;
+  padding: 2vw;
+  display: flex;
+  align-items: center;
+  color: white;
+  font-weight: 700;
+  font-size: 2.5vh;
+}
+.play {
+  background-color: var(--base-color-green);
+}
+.stop {
+  background-color: var(--color-warning);
+}
+
 </style>
